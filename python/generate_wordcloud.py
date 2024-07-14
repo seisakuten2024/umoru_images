3c743a652966e5ff3f9c36e3d2548964f3e342ba
sawada10 <ponyo-jawa-68@g.ecc.u-tokyo.ac.jp>
2024-07-14 19:55:20 +0900

    edit generate_wordcloud.py

--- python/generate_wordcloud.py
+++ python/generate_wordcloud.py
@@ -1,54 +1,48 @@
-
+from PIL import Image
 import numpy as np
-# import MeCab
-from wordcloud import WordCloud
+import MeCab
+from wordcloud import WordCloud, ImageColorGenerator
 import matplotlib.pyplot as plt
 
 #関数の設定
 def mecab_tokenizer(path):
+    # mecab= MeCab.Tagger('-Ochasen')
+    mecab = MeCab.Tagger("-d /var/lib/mecab/dic/ipadic-utf8 -Ochasen")
     token_list = []
-    # replaced_text = unicodedata.normalize("NFKC",text)
-    # replaced_text = replaced_text.upper()
-    # # replaced_text = re.sub(r'[【】 () （） 『』　「」]', '' ,replaced_text) #【】 () 「」　『』の除去
-    # # replaced_text = re.sub(r'[\[\［］\]]', ' ', replaced_text)   # ［］の除去
-    # # replaced_text = re.sub(r'[@＠]\w+', '', replaced_text)  # メンションの除去
-    # # replaced_text = re.sub(r'\d+\.*\d*', '', replaced_text) #数字を0にする
-
-    # parsed_lines = mecab.parse(replaced_text).split("\n")[:-2]
-
-    # #表層系を取得
-    # surfaces = [l.split("\t")[0] for l in parsed_lines]
-    # #品詞を取得
-    # pos = [l.split("\t")[1].split(",")[0] for l in parsed_lines]
-    # #名詞、動詞、形容詞に絞り込み
-    # target_pos = ["名詞", "動詞", "形容詞"]
-    # token_list = [t for t , p in zip(surfaces, pos) if p in target_pos]
     with open(path) as f:
         lines = f.readlines()
         for l in lines:
-            token_list.append(l.rstrip("\n"))
-    
-    #各トークンを少しスペースを空けて（' '）結合
+            node = mecab.parseToNode(l)
+            while node:
+                if node.feature.split(",")[0] == "名詞" and node.feature.split(",")[1] in ["一般", "固有名詞"]:
+                    token_list.append(node.surface)
+                node = node.next
     return ' '.join(token_list)
 
 font_path="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf"
 #関数の実行
-words = mecab_tokenizer("data/test0714.txt")
+words = mecab_tokenizer("data/test-blog.txt")
+# words = mecab_tokenizer("data/test0714.txt")
 
 #色の設定
 colormap="Paired"
 
+img_color = np.array(Image.open("data/heart.jpg"))
+# img_color = np.array(Image.open("data/heart.png"))
 wordcloud = WordCloud(
     background_color="white",
     width=800,
     height=800,
     font_path=font_path,
     colormap = colormap,
+    mask=img_color,
     stopwords=["する", "ある", "こと", "ない"],
     max_words=100,
     ).generate(words)
 
+# image_colors = ImageColorGenerator(img_color)
 plt.figure(figsize=(10, 10))
 plt.imshow(wordcloud, interpolation="bilinear")
+# plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
 plt.axis("off")
 plt.show()
