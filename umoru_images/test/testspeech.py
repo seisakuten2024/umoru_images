9bc2e5c8e04c6ff05a3f2fdc1e8be6f4468c3b27
sawada10 <ponyo-jawa-68@g.ecc.u-tokyo.ac.jp>
2024-07-24 23:48:03 +0900

    gitignoreを足した

--- umoru_images/test/testspeech.py
+++
@@ -1,9 +0,0 @@
-import rospy
-from sound_play.libsoundplay import SoundClient
-
-rospy.init_node('say_node')
-
-client = SoundClient(sound_action='robotsound', sound_topic='robotsound')
-
-client.say('hello!')
-client.say('こんにちは', voice='ja')
